907310c8703cc04e4bb0165e2c7df61d5ac11354
anastasia-romanova <romanova0304an@yandex.ru>
2020-01-15 10:27:36 +0300

    Create most of activity
    Create draft of activity Edit, History, New Event, New Fact, New Person, Settings

--- app/src/main/java/com/romanova/peopledata/MainMenu.java
+++ app/src/main/java/com/romanova/peopledata/MainMenu.java
@@ -1,7 +1,9 @@
 package com.romanova.peopledata;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onClickNewEvent(View view) {
+        Intent intent = new Intent(MainMenu.this, NewEvent.class);
+        startActivity(intent);
+    }
+
+    public void onClickNewFact(View view) {
+        Intent intent = new Intent(MainMenu.this, NewFact.class);
+        startActivity(intent);
+    }
+
+    public void onClickNewPerson(View view) {
+        Intent intent = new Intent(MainMenu.this, NewPerson.class);
+        startActivity(intent);
+    }
+
+    public void onClickEdit(View view) {
+        Intent intent = new Intent(MainMenu.this, Edit.class);
+        startActivity(intent);
     }
 }
